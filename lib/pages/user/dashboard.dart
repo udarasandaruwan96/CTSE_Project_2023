0a8b0839dc24f9cc1d0e160c24c91029eff41c84
Poojanifernando <IT20156374@my.sliit.lk>
2023-03-23 20:27:22 +0530

    anodya - create recipes

--- lib/pages/user/dashboard.dart
+++ lib/pages/user/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../screens/anodya/anodyaHome.dart';
+
+import '../screens/anodya/recipe.dart';
 import '../screens/disni/disniHome.dart';
 import '../screens/ramona/comment.dart';
 import '../screens/ramona/ramonaHome.dart';
@@ -14,84 +15,133 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-
-// methanta 
-
+// methanta
 
   @override
   Widget build(BuildContext context) {
-      //methanata................
-    
-      // ignore: prefer_const_constructors
-      return Scaffold(
-  
-// appBar: AppBar(title: Text("Grid"),),
-      body: Container(child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        
-        // ignore: sort_child_properties_last
-        child: GridView(children: [
-          InkWell(
-            //  onTap: (){
-            //    Navigator.push(context, MaterialPageRoute(builder: (context)=>UdaraHome()));
-            //  },
-            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.red,),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Icon(Icons.home,size: 50,color: Colors.white,),
-              Text("Home",style: TextStyle(color: Colors.white,fontSize: 30),)
-            ],),
-            ),
-          ),
-         InkWell(
-             onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>DisniHome()));
-            },
-           child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.yellow,),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Icon(Icons.search,size: 50,color: Colors.white,),
-              Text("Search",style: TextStyle(color: Colors.white,fontSize: 30),)
-            ],),
-            ),
-         ),
-          InkWell(
-              onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>AnodyaHome()));
-            },
-            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.green,),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Icon(Icons.settings,size: 50,color: Colors.white,),
-              Text("Settings",style: TextStyle(color: Colors.white,fontSize: 30),)
-            ],),
-            ),
-          ),
-          InkWell(
-              onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>CommentHome()));
-            },
-            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.grey,),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Icon(Icons.book,size: 45,color: Colors.white,),
-              Text("Comments",style: TextStyle(color: Colors.white,fontSize: 30),)
-            ],),
-            ),
-          ),
-        ],
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 10,crossAxisSpacing: 10),
-        ),
-      ),),
+    //methanata................
 
+    // ignore: prefer_const_constructors
+    return Scaffold(
+// appBar: AppBar(title: Text("Grid"),),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+
+          // ignore: sort_child_properties_last
+          child: GridView(
+            children: [
+              InkWell(
+                //  onTap: (){
+                //    Navigator.push(context, MaterialPageRoute(builder: (context)=>UdaraHome()));
+                //  },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.red,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.home,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Home",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => DisniHome()));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.yellow,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.search,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Search",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => addRecipe()));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.green,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.settings,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Recipes",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CommentHome()));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.grey,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.book,
+                        size: 45,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Comments",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
+          ),
+        ),
+      ),
     );
   }
-
-
 
 //button call.................................
   // void _udaraHome(BuildContext context) {
