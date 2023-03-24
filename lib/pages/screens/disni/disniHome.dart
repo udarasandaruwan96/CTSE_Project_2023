15d6d52e9038ad6218b4765036667692a2a62664
Disni Jayawickrama <it20147778@my.sliit.lk>
2023-03-25 04:59:49 +0530

    disniHome image adding- disni

--- lib/pages/screens/disni/disniHome.dart
+++ lib/pages/screens/disni/disniHome.dart
@@ -16,21 +16,15 @@ class DisniHome extends StatefulWidget {
 }
 
 class _DisniHomeState extends State<DisniHome> {
-
 // methanta .....................
-
 
   final TextEditingController _topicnameController = TextEditingController();
   final TextEditingController _dateController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
 
-
-
   final CollectionReference _addnote =
-  FirebaseFirestore.instance.collection('addnote');
+      FirebaseFirestore.instance.collection('addnote');
 
-
-  
   //add data........................................................
   Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
     await showModalBottomSheet(
@@ -42,26 +36,38 @@ class _DisniHomeState extends State<DisniHome> {
                 top: 20,
                 left: 20,
                 right: 20,
-                bottom: MediaQuery
-                    .of(ctx)
-                    .viewInsets
-                    .bottom + 20),
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Image.asset(
+                  'assets/images/noteadd.jpg',
+                  width: 300,
+                  height: 150,
+                  fit: BoxFit.fill, // Set the image to cover the entire space
+                ),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'You Can Invent Your Own Recipie!',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: _dateController,
+                  decoration: const InputDecoration(labelText: 'date'),
+                ),
                 TextField(
                   controller: _topicnameController,
                   decoration: const InputDecoration(labelText: 'topicname'),
                 ),
-
-               
                 TextField(
                   controller: _descriptionController,
                   decoration: const InputDecoration(labelText: 'description'),
                 ),
-               
-                
                 const SizedBox(
                   height: 20,
                 ),
@@ -71,16 +77,13 @@ class _DisniHomeState extends State<DisniHome> {
                     final String topicname = _topicnameController.text;
                     final String description = _descriptionController.text;
                     final String date = _dateController.text;
-                 
 
                     if (date != null) {
-                      await _addnote.add(
-                          {
-                            "topicname": topicname,
-                            "description": description,
-                            "date": date,
-                            
-                          });
+                      await _addnote.add({
+                        "topicname": topicname,
+                        "description": description,
+                        "date": date,
+                      });
 
                       _topicnameController.text = '';
                       _descriptionController.text = '';
@@ -94,19 +97,15 @@ class _DisniHomeState extends State<DisniHome> {
             ),
           );
         });
-}
-
+  }
 
   //update data...........................................................
   Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
     if (documentSnapshot != null) {
-
       _topicnameController.text = documentSnapshot['topicname'];
       _descriptionController.text = documentSnapshot['description'];
       _dateController.text = documentSnapshot['date'];
-     
     }
-
 
     await showModalBottomSheet(
         isScrollControlled: true,
@@ -134,33 +133,27 @@ class _DisniHomeState extends State<DisniHome> {
                   controller: _dateController,
                   decoration: const InputDecoration(labelText: 'Date'),
                 ),
-              
                 const SizedBox(
                   height: 20,
                 ),
                 ElevatedButton(
-                  child: const Text( 'Update'),
+                  child: const Text('Update'),
                   onPressed: () async {
                     final String topicname = _topicnameController.text;
                     final String description = _descriptionController.text;
                     final String date = _dateController.text;
-                    
-                    if (date != null) {
 
-                      await _addnote
-                          .doc(documentSnapshot!.id)
-                          .update(
-                          {
-                            "topicname": topicname,
-                            "description": description,
-                            "date": date,
-                           }
-                      );
+                    if (date != null) {
+                      await _addnote.doc(documentSnapshot!.id).update({
+                        "topicname": topicname,
+                        "description": description,
+                        "date": date,
+                      });
 
                       _topicnameController.text = '';
                       _descriptionController.text = '';
                       _dateController.text = '';
-                     
+
                       Navigator.of(context).pop();
                     }
                   },
@@ -171,65 +164,57 @@ class _DisniHomeState extends State<DisniHome> {
         });
   }
 
-
   //delete.....................................................
   Future<void> _delete(String hotlineId) async {
-    await _addnote.doc(hotlineId).delete()
-    .then((value) {
-    Get.snackbar('Success', 'Successfully Deleted');
+    await _addnote.doc(hotlineId).delete().then((value) {
+      Get.snackbar('Success', 'Successfully Deleted');
     });
     Navigator.pop(context);
-
-
-    }
-
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Note Details"),
-            ElevatedButton(
-              onPressed: () async => {
-                await FirebaseAuth.instance.signOut(),
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Login(),
-                    ),
-                    (route) => false)
-              },
-              child: Text('Logout'),
-              style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
-            )
-          ],
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Note Details"),
+              ElevatedButton(
+                onPressed: () async => {
+                  await FirebaseAuth.instance.signOut(),
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Login(),
+                      ),
+                      (route) => false)
+                },
+                child: Text('Logout'),
+                style: ElevatedButton.styleFrom(primary: Colors.blueGrey),
+              )
+            ],
+          ),
         ),
-      ),
 
-
-      //......body....................................................
+        //......body....................................................
         body: StreamBuilder(
           stream: _addnote.snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot)
-
-          {
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
             if (streamSnapshot.hasData) {
               return ListView.builder(
                 itemCount: streamSnapshot.data!.docs.length,
-                itemBuilder: (context, index) {final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
 
-                return Card(
+                  return Card(
                     shadowColor: Colors.purpleAccent,
                     margin: const EdgeInsets.all(13),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(9.9),
                     ),
                     child: ListTile(
-                      
                       title: Text(
                         documentSnapshot['topicname'].toString(),
                         style: TextStyle(
@@ -238,103 +223,90 @@ class _DisniHomeState extends State<DisniHome> {
                           fontWeight: FontWeight.w600,
                         ),
                       ),
-                      
                       subtitle: Column(
                         children: [
                           Text(
-                        documentSnapshot['description'].toString(),
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w600,
-
-                          
-                        ),
-                       
-                      ),
-                        Align(alignment: Alignment.topRight, child:Text(
-                        documentSnapshot['date'].toString(),
-                        style: TextStyle(
-                          fontSize: 10,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w600,
-                          
-
-                        
-                        ),
-                       
-                      ),
-                        ),
-
+                            documentSnapshot['description'].toString(),
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              documentSnapshot['date'].toString(),
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
                         ],
-                      ), 
-
-                      
-
-                      
+                      ),
                       trailing: SizedBox(
                         width: 130,
                         child: Row(
-                            children: [
-                        TextButton(
-                        style: TextButton.styleFrom(
-                        backgroundColor: Color(0xEFEFEFFF),
-                        shape: CircleBorder(),
-                      ),
-                      child: Icon(
-                        Icons.edit,
-                        color: Color.fromARGB(255, 33, 115, 209),
-                      ),
-                      onPressed: () => _update(documentSnapshot),
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Color(0xEFEFEFFF),
-                        shape: CircleBorder(),
-                      ),
-                      child: Icon(
-                        Icons.delete,
-                        color: Color.fromARGB(255, 224, 68, 68),
-                      ),
-                      onPressed: () {
-                        // Delete Confirmation Message
-                        // set up the buttons
-                        Widget cancelButton = TextButton(
-                          child: Text("Cancel"),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        );
-                        Widget continueButton = TextButton(
-                          child: Text("Ok"),
-                          onPressed: () => _delete(documentSnapshot.id),
-                        );
+                          children: [
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: Color(0xEFEFEFFF),
+                                shape: CircleBorder(),
+                              ),
+                              child: Icon(
+                                Icons.edit,
+                                color: Color.fromARGB(255, 33, 115, 209),
+                              ),
+                              onPressed: () => _update(documentSnapshot),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: Color(0xEFEFEFFF),
+                                shape: CircleBorder(),
+                              ),
+                              child: Icon(
+                                Icons.delete,
+                                color: Color.fromARGB(255, 224, 68, 68),
+                              ),
+                              onPressed: () {
+                                // Delete Confirmation Message
+                                // set up the buttons
+                                Widget cancelButton = TextButton(
+                                  child: Text("Cancel"),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                );
+                                Widget continueButton = TextButton(
+                                  child: Text("Ok"),
+                                  onPressed: () => _delete(documentSnapshot.id),
+                                );
 
-                        // set up the AlertDialog
-                        AlertDialog alert = AlertDialog(
-                          title: Text("Note Details"),
-                          content: Text("Are you sure want to delete?"),
-                          actions: [
-                            cancelButton,
-                            continueButton,
+                                // set up the AlertDialog
+                                AlertDialog alert = AlertDialog(
+                                  title: Text("Note Details"),
+                                  content: Text("Are you sure want to delete?"),
+                                  actions: [
+                                    cancelButton,
+                                    continueButton,
+                                  ],
+                                );
+                                // show the dialog
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return alert;
+                                  },
+                                );
+                              },
+                            ),
                           ],
-                        );
-                        // show the dialog
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return alert;
-                          },
-                        );
-                      },
-                    ),
-
-
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                );
+                  );
                 },
               );
             }
@@ -344,18 +316,12 @@ class _DisniHomeState extends State<DisniHome> {
             );
           },
         ),
-       
-       
-      // Add new..........................................................................................
+
+        // Add new..........................................................................................
         floatingActionButton: FloatingActionButton(
           onPressed: () => _create(),
           child: const Icon(Icons.add),
-
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat
-  
-     
-    ); 
-
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 }
