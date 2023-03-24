c865519701fb25d648a67d3bd71dfc8054fe1b83
IT20405090 <IT20405090@my.sliit.lk>
2023-03-24 09:34:47 +0530

    text alignments fixed in home

--- lib/pages/screens/ramona/comment.dart
+++ lib/pages/screens/ramona/comment.dart
@@ -24,9 +24,16 @@ class _CommentHomeState extends State<CommentHome> {
           const SizedBox(height: 30),
           const Text('We would love your feedback', style: TextStyle(fontSize: 20)),
           const SizedBox(height: 20),
-          const Text('We strive to make cooking for you more simpler and more easy with our dishes and cutting-edge application', style: TextStyle(fontSize: 15), textAlign: TextAlign.center,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,),
+           Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: const Text(
+                'We strive to make cooking for you more simpler and more easy with our dishes and cutting-edge application',
+                style: TextStyle(fontSize: 15),
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+           ),
           SizedBox(height: 30),
           ElevatedButton(
             onPressed: () {
